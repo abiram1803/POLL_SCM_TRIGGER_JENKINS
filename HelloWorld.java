796796ca6c6d12f2b4d184d83d001f772e6cfa23
abiram1803 <rshreeabiram@gmail.com>
2026-09-22 20:46:36 +0530

    Testing Github webhook real-time trigger

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Poll SCM is working now!");
+        System.out.println("Webhook Github");
     }
 }
